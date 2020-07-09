bc24be53cee1ae9ad915e3534939bf4cf03eeaac
Umar <umar@vimix.ao>
2020-07-09 18:44:04 +0100

    Applied reccomendations 1-3 based on audit report

--- contracts/SNG.sol
+++ contracts/SNG.sol
@@ -10,11 +10,11 @@ contract SanuGold is Initializable, ERC20PresetMinterPauserUpgradeSafe {
     // FEE CONTROLLER DATA
     // fee decimals is only set for informational purposes.
     // 1 feeRate = .000001 oz of gold
-    uint8 public constant feeDecimals = 6;
+    uint8 public constant FEE_DECIMALS = 6;
 
     // feeRate is measured in 100th of a basis point (parts per 1,000,000)
     // ex: a fee rate of 200 = 0.02% of an oz of gold
-    uint256 public constant feeParts = 1000000;
+    uint256 public constant FEE_PARTS = 1000000;
     uint256 public feeRate;
     address public feeController;
     address public feeRecipient;
@@ -54,13 +54,12 @@ contract SanuGold is Initializable, ERC20PresetMinterPauserUpgradeSafe {
     }
 
     modifier onlyOwner() {
-      require(msg.sender == owner, "only FeeController");
+      require(msg.sender == owner, "only Owner");
       _;
     }
 
     // Overriding the initialize function to initialize more variables
     function initialize(string memory name, string memory symbol)virtual public override{
-        feeRate = 0;
         feeController = msg.sender;
         feeRecipient = msg.sender;
         owner = msg.sender;
@@ -80,7 +79,7 @@ contract SanuGold is Initializable, ERC20PresetMinterPauserUpgradeSafe {
 
         super.transfer(to, principle);
 
-        if(fee > 0){
+        if(fee != 0){
             super.transfer(feeRecipient, fee);
             emit FeeCollected(msg.sender, feeRecipient, fee);
         }
@@ -158,7 +157,7 @@ contract SanuGold is Initializable, ERC20PresetMinterPauserUpgradeSafe {
      * @param _newFeeRate The new fee rate to collect as transfer fees for transfers.
      */
     function setFeeRate(uint256 _newFeeRate) public onlyFeeController {
-        require(_newFeeRate <= feeParts, "cannot set fee rate above 100%");
+        require(_newFeeRate <= FEE_PARTS, "cannot set fee rate above 100%");
         uint256 _oldFeeRate = feeRate;
         feeRate = _newFeeRate;
         emit FeeRateSet(_oldFeeRate, feeRate);
@@ -189,7 +188,7 @@ contract SanuGold is Initializable, ERC20PresetMinterPauserUpgradeSafe {
 
     /**
     * @dev Gets a fee for a given value
-    * ex: given feeRate = 200 and feeParts = 1,000,000 then getFeeFor(10000) = 2
+    * ex: given feeRate = 200 and FEE_PARTS = 1,000,000 then getFeeFor(10000) = 2
     * @param value The amount to get the fee for.
     */
     function getFeeFor(uint256 value) public view returns (uint256) {
@@ -197,6 +196,6 @@ contract SanuGold is Initializable, ERC20PresetMinterPauserUpgradeSafe {
             return 0;
         }
 
-        return value.mul(feeRate).div(feeParts);
+        return value.mul(feeRate).div(FEE_PARTS);
     }
 }
